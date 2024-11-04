c4666678ca80631951969377af3afcafc6bc61e8
firespark <fireleit@gmail.com>
2024-11-04 19:01:02 +0400

    feat: add task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,18 @@
+import { createReadStream } from 'node:fs';
+
+const read = async () => {
+    const folderPath = import.meta.dirname;
+    const filePath = `${folderPath}/text.txt`;
+
+    const stream = await createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+        process.stdout.write(`${chunk}\n`);
+    });
+
+    stream.on('error', (err) => {
+        console.error(`Error reading file: ${err.message}`);
+    });
+};
+
+await read();
