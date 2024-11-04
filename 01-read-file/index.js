3748adb056a9276866a2801b37eabbda987aa8f1
firespark <fireleit@gmail.com>
2024-11-04 19:27:57 +0400

    refactor: use commonjs instead of es modules

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,10 @@
-import { createReadStream } from 'node:fs';
+const fs = require('fs');
+const path = require('path');
 
-const read = async () => {
-    const folderPath = import.meta.dirname;
-    const filePath = `${folderPath}/text.txt`;
+function read() {
+    const filePath = path.join(__dirname, 'text.txt');
 
-    const stream = await createReadStream(filePath);
+    const stream = fs.createReadStream(filePath);
 
     stream.on('data', (chunk) => {
         process.stdout.write(`${chunk}\n`);
@@ -13,6 +13,6 @@ const read = async () => {
     stream.on('error', (err) => {
         console.error(`Error reading file: ${err.message}`);
     });
-};
+}
 
-await read();
+read();
